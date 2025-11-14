7f8147fc918b99b54cc1e825d99d54ddb5ea684f
jenerCruz <pamecoaxaca.telcdl@gmail.com>
2025-11-14 16:45:24 -0600

    android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.app3.com',
+  appName: 'app3',
+  webDir: 'www'
+};
+
+export default config;
